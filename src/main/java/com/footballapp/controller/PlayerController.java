b27d62a2f912a2be2b91584d13c4d0a946503b29
ursula Paul <ursulapaul31@gmail.com>
2025-08-27 07:57:11 +0100

    Modified the PlayerController

--- src/main/java/com/footballapp/controller/PlayerController.java
+++ src/main/java/com/footballapp/controller/PlayerController.java
@@ -4,3 +4,36 @@ import com.footballapp.entity.Player;
 import com.footballapp.service.PlayerService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/players")
+public class PlayerController{
+    private final PlayerService service;
+
+    public PlayerController(PlayerService service){
+        this.service = service;
+    }
+
+
+    @PostMapping
+    public Player addPlayer(@RequestBody Player player){
+        return service.savePlayer(player);
+    }
+
+    @GetMapping
+    public List<Player> getAllPlayers(){
+        return service.getAllPlayers();
+    }
+
+    @GetMapping("/{id}")
+        public Player getPlayer(@PathVariable Long id) {
+        return service.getPlayerBYID(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deletePlayer(@PathVariable Long id){
+        service.deletePlayer(id);
+        return "Player deleted with id: " + id;
+    }
+}
