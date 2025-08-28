1922b51a94574f2da25a332ddb6c62a2f6090162
ursula Paul <ursulapaul31@gmail.com>
2025-08-28 10:55:24 +0100

    feat: add team-player feature, playerCode, validations, switching team

--- src/main/java/com/footballapp/controller/TeamController.java
+++ src/main/java/com/footballapp/controller/TeamController.java
@@ -1,8 +1,35 @@
 package com.footballapp.controller;
 
-import com.footballapp.entity.Controller
+import com.footballapp.entity.Team;
+import com.footballapp.service.TeamService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-public class TeamController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/teams")
+public class TeamController{
+    private final TeamService teamService;
 
+    public TeamController(TeamService teamService){
+        this.teamService =teamService;
+    }
+
+    // Get all teams
+
+    @GetMapping
+    public ResponseEntity<List<Team>> getAllTeams(){
+        List <Team> teams =teamService.getAllTeams();
+        return new ResponseEntity<>(teams, HttpStatus.OK);
+    }
+
+    //Get a specific team
+    @GetMapping("/{id}")
+    public ResponseEntity<Team> getTeamById(@PathVariable Long id){
+        Team team = teamService.getTeamByID(id);
+        return new ResponseEntity<>(team, HttpStatus.OK);
+    }
 }
+
--- src/main/java/com/footballapp/entity/Team.java
+++ src/main/java/com/footballapp/entity/Team.java
@@ -1,4 +1,30 @@
 package com.footballapp.entity;
 
-public class Team {
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class  Team{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column (unique= true, nullable = false)
+    private String name;
+
+    @OneToMany (mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Player> players = new ArrayList<>();
+
+
+    // Getters and Setters
+
+    public Long getId() {return id;}
+    public String getName() { return name;};
+    public void setName(String Name) {this.name = name;}
+    public List<Player>getPlayers (){return players;}
+
+
 }
+
+
